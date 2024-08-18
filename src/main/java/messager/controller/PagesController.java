450809a27ebc62c2e4be30a75366e4a96b8849ef
Matvey <matvv009@outlook.com>
2024-08-18 21:16:15 +0300

    Bug fix in profile pages. Add return  button in settings page

--- src/main/java/messager/controller/PagesController.java
+++ src/main/java/messager/controller/PagesController.java
@@ -1,14 +1,20 @@
 package messager.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import messager.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
 
+@RequiredArgsConstructor
 @Controller
 public class PagesController {
+
+
+    private final UserService userService;
 
 
     @GetMapping("/messager")
@@ -36,6 +42,9 @@ public class PagesController {
 
     @GetMapping("/profile/{username}")
     public String profile(Principal principal, @PathVariable String username) {
+        if (userService.find(username) == null) {
+            return "EmptyPage";
+        }
         if (principal.getName().equals(username)) {
             return "profile";
         }
